aa4adcd354f661af38c0b7c4958c787189cbf354
nani123-hub <nithinvinnakota10@gmail.com>
2025-09-20 16:13:25 +0530

    retriever updated

---
+++ prod_assistant/retriever/retrieval.py
@@ -0,0 +1,30 @@
+import os
+from langchain_astradb import AstraDBVectorStore
+from typing import List
+from langchain_core.documents import Document
+from utils.config_loader import load_config
+from utils.model_loader import ModelLoader
+from dotenv import load_dotenv
+
+class Retriever:
+    def __init__(self):
+        pass
+    
+    def _load_env_variables(self):
+        # Load environment variables
+        pass
+    
+    def load_retriver(self):
+        # Load the retriever model
+        pass
+    
+    def call_retriever(self,user_query):
+        # Call the retriever with a query
+        pass
+    
+if __name__ == "__main__":
+    retriever_obj = Retriever()
+    user_query = "can you suggest good budget laptops?"
+    results = retriever_obj.call_retriever(user_query)
+    for idx, doc in enumerate(results,1):
+        print(f"Result {idx}: {doc.page_content}\nMetadata: {doc.metadata}\n")
